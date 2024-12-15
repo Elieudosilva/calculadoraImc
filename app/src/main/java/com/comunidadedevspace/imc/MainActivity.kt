a0c7d8341e2eca2a86e0333cbe580e1da8096703
Elieudo Silva <elieudosilva95@gmail.com>
2024-12-15 13:46:49 -0300

    Clean code

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -45,9 +45,6 @@ class MainActivity : AppCompatActivity() {
                 intent.putExtra(KEY_RESULT_IMC,resultado)
                 startActivity(intent)
 
-                // Cores
-                // EdiText background + Icone
-                // Gradiente + icone + titulo + descricao
             }
 
         }
